e3c0fab1064bfb7e1ed2c5483f2e798bfecb5677
AndriusDulevicius <andriusdulevicius@email.com>
2021-10-01 19:37:17 +0300

    billing options secion refactored

---
+++ src/pages/checkout/elements/OptionCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import { DataTypes } from '../sections/OptionSelection';
+import { H2, SmallCard, FlexWrapper, H5, Svg, TextWrapper, SmallText, PrimaryButton } from 'components';
+import { dark_text, green, purple, orange, blue } from 'styles/colors';
+
+export const OptionCard: React.FC<DataTypes> = ({ product_key, original_price, final_price, daily_price }) => {
+  const discount = 100 - (final_price / original_price) * 100;
+  return (
+    <SmallCard margin='1rem auto' padding='1rem' textAlign='center' key={product_key}>
+      <FlexWrapper flexWrap='no-wrap' gap='0.5rem'>
+        {product_key.includes('1') ? (
+          <>
+            <H5>Monthly</H5>
+            <BannerCard colorProp={purple}>
+              <Svg src='purple_star' />
+              <StyledTextWrapper color={purple}>Most popular</StyledTextWrapper>
+            </BannerCard>
+          </>
+        ) : (
+          <>
+            <H5>One-time payment</H5>
+            <BannerCard colorProp={green}>
+              <TextWrapper color={green}>Best value</TextWrapper>
+            </BannerCard>
+          </>
+        )}
+      </FlexWrapper>
+      <H2 margin='0.5rem 0 0'>
+        ${daily_price} <TextWrapper fontWeight={400}>/ day</TextWrapper>
+      </H2>
+      <SmallText margin='0.5rem 0'>
+        <FullPrice> {`$${original_price}`}</FullPrice>
+        <DiscountedPrice>{`$${final_price} `}</DiscountedPrice>
+        {product_key.includes('1') ? 'billed every month' : 'One-time payment'}
+      </SmallText>
+      <PrimaryButton maxWidth='100%' colorProp={blue}>
+        Start now <TextWrapper fontWeight={400}>(Save {discount.toFixed()}%)</TextWrapper>
+      </PrimaryButton>
+    </SmallCard>
+  );
+};
+
+const BannerCard = styled.div<{ colorProp: any }>`
+  padding: 0.2rem 0.5rem;
+  background-color: transparent;
+  border: 0.0625rem solid ${({ colorProp }) => colorProp};
+  border-radius: 0.312rem;
+`;
+
+const FullPrice = styled.span`
+  text-decoration: line-through;
+  color: ${dark_text};
+  margin-right: 0.3rem;
+`;
+
+const DiscountedPrice = styled.span`
+  color: ${orange};
+  font-weight: 700;
+`;
+
+const StyledTextWrapper = styled(TextWrapper)`
+  margin-left: 0.3rem;
+`;
--- src/pages/checkout/sections/OptionSelection.tsx
+++ src/pages/checkout/sections/OptionSelection.tsx
@@ -1,24 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { getPlansData } from 'apis/fetch';
-import {
-  AbsoluteBox,
-  Container,
-  ContainerSmall,
-  H2,
-  Image,
-  SmallCard,
-  FlexWrapper,
-  H5,
-  Svg,
-  TextWrapper,
-  H1,
-  SmallText,
-  PrimaryButton,
-  H3,
-  CaptionText,
-} from 'components';
 import styled from 'styled-components';
-import { grey_white, dark_text, green, purple, orange, blue } from 'styles/colors';
+import { getPlansData } from 'apis/fetch';
+import { OptionCard } from '../elements/OptionCard';
+import { AbsoluteBox, Container, ContainerSmall, H2, Image, FlexWrapper, H3, CaptionText } from 'components';
+import { grey_white, blue } from 'styles/colors';
 import { useQuery } from 'styles/breakpoints';
 
 export interface DataTypes {
@@ -47,44 +32,11 @@ export const OptionSelection: React.FC = () => {
         </AbsoluteBox>
       )}
       <Container margin='2rem auto' maxWidth='50rem'>
-        {isMobile ? <H3>Chose your personal 3-month plan:</H3> : <H2>Select billint option</H2>}
+        {isMobile ? <H3>Chose your personal 3-month plan:</H3> : <H2>Select billing option</H2>}
         <FlexWrapper justifyContent={isMobile ? 'column' : 'row'}>
-          {plans.map(({ product_key, final_price, daily_price, original_price }) => {
-            const discount = 100 - (final_price / original_price) * 100;
-            return (
-              <SmallCard margin='1rem auto' padding='1rem' textAlign='center' key={product_key}>
-                <FlexWrapper flexWrap='no-wrap' gap='0.5rem'>
-                  {product_key.includes('1') ? (
-                    <>
-                      <H5>Monthly</H5>
-                      <BannerCard colorProp={purple}>
-                        <Svg src='purple_star' />
-                        <StyledTextWrapper color={purple}>Most popular</StyledTextWrapper>
-                      </BannerCard>
-                    </>
-                  ) : (
-                    <>
-                      <H5>One-time payment</H5>
-                      <BannerCard colorProp={green}>
-                        <TextWrapper color={green}>Best value</TextWrapper>
-                      </BannerCard>
-                    </>
-                  )}
-                </FlexWrapper>
-                <H1 margin='0.5rem 0 0'>
-                  ${daily_price} <TextWrapper fontWeight={400}>/ day</TextWrapper>
-                </H1>
-                <SmallText margin='1rem 0'>
-                  <FullPrice> {`$${original_price}`}</FullPrice>
-                  <DiscountedPrice>{`$${final_price} `}</DiscountedPrice>
-                  {product_key.includes('1') ? 'billed every month' : 'One-time payment'}
-                </SmallText>
-                <PrimaryButton maxWidth='100%' colorProp={blue}>
-                  Start now <TextWrapper fontWeight={400}>(Save {discount.toFixed()}%)</TextWrapper>
-                </PrimaryButton>
-              </SmallCard>
-            );
-          })}
+          {plans.map((plan) => (
+            <OptionCard {...plan} />
+          ))}
         </FlexWrapper>
         <ContainerSmall margin='0 auto' maxWidth='70%'>
           <CaptionText textAlign='center'>
@@ -104,28 +56,6 @@ const StyledSection = styled.section`
   position: relative;
   padding: 1rem;
   background-color: ${grey_white};
-`;
-
-const BannerCard = styled.div<{ colorProp: any }>`
-  padding: 0.2rem 0.5rem;
-  background-color: transparent;
-  border: 0.0625rem solid ${({ colorProp }) => colorProp};
-  border-radius: 0.312rem;
-`;
-
-const FullPrice = styled.span`
-  text-decoration: line-through;
-  color: ${dark_text};
-  margin-right: 0.3rem;
-`;
-
-const DiscountedPrice = styled.span`
-  color: ${orange};
-  font-weight: 700;
-`;
-
-const StyledTextWrapper = styled(TextWrapper)`
-  margin-left: 0.3rem;
 `;
 
 const BlueText = styled.span`
